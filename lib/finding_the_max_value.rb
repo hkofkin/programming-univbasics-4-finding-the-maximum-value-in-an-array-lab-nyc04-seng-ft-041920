20e0662e64a8a71326393bad2041ffae7a206b19
Hannah Kofkin <hkofkin@gmail.com>
2020-04-22 20:10:53 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,8 +3,10 @@ def find_max_value(array)
   count = 0 
   max_value = -1
   while count < array.length do 
-    new_array = array.sort
+    if max_value < array[count]
+      max_value = array[count]
+    end
     count += 1
   end
-  return new_array[-1]
+  max_value
 end
